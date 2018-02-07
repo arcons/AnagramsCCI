b42199e8d1d6683370386e5b6d6d78926e85f6bc
Alex Consunji <a.consunji@knights.ucf.edu>
2018-02-07 15:35:40 -0500

    Add project files.

---
+++ AnagramsCCI/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+class Solution
+{
+
+    static void Main(String[] args)
+    {
+        string a = Console.ReadLine();
+        string b = Console.ReadLine();
+
+        // elements automatically initialized to 0
+        int[] letterCounts = new int[26];
+
+        foreach (char c in a) { letterCounts[c - 'a']++; }
+        foreach (char c in b) { letterCounts[c - 'a']--; }
+        Console.WriteLine(letterCounts.Sum(x => Math.Abs(x)));
+    }
+}
